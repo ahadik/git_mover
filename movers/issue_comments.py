e5c880895539c6ec714c1aaaa8e7e8d9ae8da0ff
Michał Łukaszewski <mlukaszewski@gmail.com>
2017-11-17 13:09:32 +0100

    added review handler for PR's

--- movers/issue_comments.py
+++ movers/issue_comments.py
@@ -23,27 +23,26 @@ def create_issue_comments(issue_comments, issue_comment_url, credentials):
             message = json.loads(r.text)
             continue
 
-def create_review_comments(review_comments, review_comment_url, credentials):
+def create_review_comments(review_comments, review_comment_url, credentials, source_pr_reviews):
     if not review_comments:
         return False
-
-    print "Creating review comment for pull request"
-    print "PR review comment url: %s" % review_comment_url
 
     comments_map = {}
 
     for review_comment in review_comments:
+        print "Creating review comment for pull request"
+        print "Source comment ID: %d" % int(review_comment.get("id"))
         review_comment_desc = {
             "body": add_user_to_text(review_comment["user"]["login"], review_comment["body"])
         }
 
-        if review_comment.get("in_reply_to_id", False) and comments_map[review_comment.get("in_reply_to_id")] != None:
+        if review_comment.get("in_reply_to_id", False) and comments_map.get(review_comment.get("in_reply_to_id", 0), False):
             review_comment_desc["in_reply_to_id"] = comments_map[review_comment.get("in_reply_to_id")]
         else:
             review_comment_desc["commit_id"] = review_comment["commit_id"]
             review_comment_desc["path"] = review_comment["path"]
-            review_comment_desc["position"] = review_comment["position"]
-
+            if review_comment.get("position", None) != None:
+                review_comment_desc["position"] = int(review_comment.get("position"))
 
         r = post_req(review_comment_url, json.dumps(review_comment_desc), credentials)
 
@@ -54,4 +53,44 @@ def create_review_comments(review_comments, review_comment_url, credentials):
             #get the message from the response
             continue
         else:
-            comments_map[review_comment.get("id")] = message.get("id")
+            print "New comment ID: %d" % int(message.get("id"))
+            comments_map[review_comment.get("id")] = int(message.get("id"))
+            processed_source_pr_reviews = update_review(message, credentials, review_comment.get("pull_request_review_id"), source_pr_reviews)
+
+            process_reviews(message.get("pull_request_url"), processed_source_pr_reviews, credentials)
+
+def update_review(message, credentials, source_pr_review_id, source_pr_reviews):
+    dest_pr_review_url = message.get("pull_request_url") + "/reviews/%d" % int(message.get("pull_request_review_id"))
+    dest_pr_review = get_data(dest_pr_review_url, credentials)
+    if dest_pr_review:
+        print "Updating review for review comment"
+        source_pr_review = source_pr_reviews.get(int(source_pr_review_id))
+        dest_pr_review["state"] = source_pr_review["state"]
+        dest_pr_review["body"] = source_pr_review["body"]
+
+        r = post_req(dest_pr_review_url + "/events", json.dumps(dest_pr_review), credentials)
+        status = check_res(r)
+        message = json.loads(r.text)
+
+        if status:
+            source_pr_reviews[source_pr_review_id] = None
+
+    return source_pr_reviews
+
+def process_reviews(dest_pr_url, source_reviews, credentials):
+    for key, value in source_reviews.iteritems():
+        if value == None:
+            continue
+        
+        dest_review = {}
+        dest_review["commit_id"] = value["commit_id"]
+        dest_review["body"] = value["body"]
+        dest_review["event"] = value["event"]
+
+        r = post_req(dest_pr_url + "/reviews", json.dumps(dest_review), credentials)
+        status = check_res(r)
+        message = json.loads(r.text)
+
+        if not status:
+            print "FAILED: create review for PR %s " % dest_pr_url
+
--- movers/pull_requests.py
+++ movers/pull_requests.py
@@ -3,20 +3,20 @@ from libs import *
 from movers.issue_comments import create_issue_comments, download_issue_comments, create_review_comments
 
 def process_pull_request(issue, source_credentials, credentials, destination_url, destination, milestones, labels, milestone_map):
+    print "Processing PR"
     pull_request_data = issue.get("pull_request")
     pull_request = get_data(pull_request_data.get("url"), source_credentials)
     if not pull_request:
         return False
     else:
-        return create_pull_request(pull_request, source_credentials, credentials, destination_url, destination, milestones, labels, milestone_map)
+        return create_pull_request(pull_request, source_credentials, credentials, destination_url, destination, milestones, labels, milestone_map, issue)
 
 def download_pull_request(pull_request_url, credentials):
     print "Getting source PR"
     return get_data(pull_request_url, credentials)
 
-def create_pull_request(pull_request, source_credentials, credentials, destination_url, destination, milestones, labels, milestone_map):
+def create_pull_request(pull_request, source_credentials, credentials, destination_url, destination, milestones, labels, milestone_map, issue):
     if not is_branch_exists(pull_request["head"]["ref"], credentials, destination_url, destination):
-        print "Branch '"+pull_request["head"]["ref"]+"' not exists anymore"
         return False
 
     url = destination_url+"repos/"+destination+"/pulls"
@@ -27,9 +27,7 @@ def create_pull_request(pull_request, source_credentials, credentials, destinati
     pull_request_dest["base"] = pull_request["base"]["ref"]
     pull_request_dest["body"] = add_user_to_text(pull_request["user"]["login"], pull_request["body"])
 
-    print "preparing to create pr"
-    print pull_request_dest
-
+    print "Creating PR"
     r = post_req(url, json.dumps(pull_request_dest), credentials)
     status = check_res(r)
     
@@ -63,12 +61,15 @@ def create_pull_request(pull_request, source_credentials, credentials, destinati
         issue_comments = get_data(pull_request.get("comments_url"), source_credentials)
         create_issue_comments(issue_comments, returned_pull_request.get("comments_url"), credentials)
 
+    source_pr_reviews = get_pr_reviews(pull_request.get("url"), source_credentials)
+
     if pull_request.get("review_comments", 0) > 0:
         print "Adding PR review comments"
-        review_comments = get_data(pull_request.get("review_comments_url"), source_credentials)
-        create_review_comments(review_comments, returned_pull_request.get("review_comments_url"), credentials)
+        review_comments_url = pull_request.get("review_comments_url") + "?sort=created&direction=asc"
+        review_comments = get_data(review_comments_url, source_credentials)
+        create_review_comments(review_comments, returned_pull_request.get("review_comments_url"), credentials, source_pr_reviews)
 
-    update_pr_issue(returned_pull_request, pull_request, credentials, source_credentials, milestones, labels, milestone_map)
+    update_pr_issue(returned_pull_request, pull_request, credentials, milestones, labels, milestone_map, issue)
 
     print "PR created %d -> %d" % (int(pull_request["number"]), int(returned_pull_request.get("number")))
 
@@ -91,6 +92,8 @@ def is_branch_exists(branch_name, credentials, destination_url, destination):
     if branch_name == "master":
         return False
 
+    print "Checking is branch '%s' exists..." % branch_name
+
     url = destination_url+"repos/"+destination+"/branches/" + branch_name
     r = get_req(url, credentials)
     status = check_res(r)
@@ -100,10 +103,9 @@ def is_branch_exists(branch_name, credentials, destination_url, destination):
     else:
         return False
 
-def update_pr_issue(returned_pull_request, pull_request, credentials, source_credentials, milestones, labels, milestone_map):
+def update_pr_issue(returned_pull_request, pull_request, credentials, milestones, labels, milestone_map, source_issue_data):
     # get state and labels for PR Issue
     issue_data = get_data(returned_pull_request["_links"]["issue"]["href"], credentials)
-    source_issue_data = get_data(pull_request["_links"]["issue"]["href"], source_credentials)
     if pull_request.get("state", "open") == "closed":
         print "Closing PR"
         issue_data["state"] = "closed"
@@ -118,3 +120,20 @@ def update_pr_issue(returned_pull_request, pull_request, credentials, source_cre
             issue_data["milestone"] = milestone_map[source_issue_data["milestone"]["number"]]
     print "Updating PR Issue"
     patch_req(issue_data["url"], json.dumps(issue_data), credentials)
+
+def get_pr_reviews(source_pr_url, credentials):
+    source_pr_reviews_url = source_pr_url + "/reviews"
+    source_reviews = get_data(source_pr_reviews_url, credentials)
+    
+    if source_reviews:
+        reviews = {}
+        for review in source_reviews:
+            review_skeleton = {}
+            review_skeleton["user"] = get_destination_username(review["user"]["login"])
+            review_skeleton["body"] = add_user_to_text(review["user"]["login"], review["body"])
+            review_skeleton["state"] = review["state"]
+            reviews[int(review.get("id"))] = review_skeleton
+
+        return reviews
+    else:
+        return False
