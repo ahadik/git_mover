7acfedd2fc424ba2a9076a961720673335f0b2a6
Wooseung Sim <wsim@freshbooks.com>
2019-06-20 13:25:57 -0400

    simplified script options

--- README.md
+++ README.md
@@ -12,55 +12,77 @@ Install them with pip: `pip install requests argparse`.
 
 ## Usage
 ```bash
-$ git-mover.py [-h] [--destinationToken [DESTINATIONTOKEN]]
-                    [--destinationUserName [DESTINATIONUSERNAME]]
-                    [--sourceRoot [SOURCEROOT]]
-                    [--destinationRoot [DESTINATIONROOT]] [--milestones]
-                    [--labels] [--issues] [--repo] [--clone]
-                    user_name token source_repo destination_repo
+$ python git-mover.py 
+              [-h] 
+              [--repo] [--githubData] [--clone]
+              [--sourceUserName [SOURCEUSERNAME]] 
+              [--sourceToken [SOURCETOKEN]]
+              [--destinationUserName [DESTINATIONUSERNAME]]
+              [--destinationToken [DESTINATIONTOKEN]]
+              [--ssh]
+              [--sourceHost [SOURCEHOST]] [--destinationHost [DESTINATIONHOST]]
+              [--inheritVisibility]
+              source_repo destination_repo
 ```
 
 ### Example
-##### Move GHE repository dev/gcp to github.com with milestones, labels, and issues (when repository freshbooks/gcp doesn't exist)
+##### Move GHE repository dev/gcp to github.com with milestones, labels, and issues (when repository freshbooks/gcp doesn't exist on target github)
 ```bash
-$ git-mover.py wsim 9asdklef91239dbfk0123123 dev/gcp freshbooks/gcp --dt 012kfjasdf0123jkasdf -dun wooseung-sim -m -l -i -r
+$ git-mover.py dev/gcp freshbooks/gcp -r --sun <source github username> --st <source github auth token> -dun <destination github username> --dt <desitination github auth token>
 ```
 
 ##### Move GHE repository dev/gcp to github.com with milestones, labels, and issues (when empty repository freshbooks/gcp exist)
 ```bash
-$ git-mover.py wsim 9asdklef91239dbfk0123123 dev/gcp freshbooks/gcp --dt 012kfjasdf0123jkasdf -dun wooseung-sim -m -l -i -c
+$ git-mover.py dev/gcp freshbooks/gcp -c -gd --sun <source github username> --st <source github auth token> -dun <destination github username> --dt <desitination github auth token>
+```
+
+##### Move GHE repository dev/gcp to github.com repository only (commits/branches/tags) when repository freshbooks/gcp exists
+```bash
+$ git-mover.py dev/gcp freshbooks/gcp -c --sun <source github username> --st <source github auth token> -dun <destination github username> --dt <desitination github auth token>
+```
+
+##### Move GHE repository dev/gcp to github.com repository only (commits/branches/tags) with SSH connection when repository freshbooks/gcp exists
+```bash
+$ git-mover.py dev/gcp freshbooks/gcp -c -s
 ```
 
 For authentication, GitMover uses a personal access token, which can be generated in your GitHub Profile settings.
 
 ### Positional Arguments
-  `user_name`: Your GitHub (public or enterprise) username: name@email.com
-  
-  `token`: Your GitHub (public or enterprise) personal access token
-  
   `source_repo`: the team and repo to migrate from: `<team_name>/<repo_name>`
   
   `destination_repo`: the team and repo to migrate to: `<team_name>/<repo_name>`
   
 ### Optional Arguments
-  `-h, --help`: show this help message and exit
-  
-  `--sourceRoot [SOURCEROOT], -sr [SOURCEROOT]`: The GitHub domain to migrate from. Defaults to https://www.github.com. For GitHub enterprise customers, enter the domain for your GitHub installation.
-  
-  `--destinationRoot [DESTINATIONROOT], -dr [DESTINATIONROOT]`: The GitHub domain to migrate to. Defaults to https://github.2ndsiteinc.com. For GitHub enterprise customers, enter the domain for your GitHub installation.
-  
-  `--destinationToken [DESTINATIONTOKEN], -dt [DESTINATIONTOKEN]`: Your personal access token for the destination account, if you are migrating between different GitHub installations.
-  
-  `--destinationUserName [DESTINATIONUSERNAME], -dun [DESTINATIONUSERNAME]`: Username (email address) for destination account, if you are migrating between different GitHub installations.
-  
-  `--milestones, -m`: Toggle on Milestone migration.
-  
-  `--labels, -l`: Toggle on Label migration.
-  
-  `--issues, -i`: Toggle on Issue migration.
 
-  `--repo, -r`: Toggle on create new repository on destination GitHub. By default it creates private repository. Create repo contains clone the repo automatically.
+#### GitHub host options (be default source is 2ndsiteinc GitHub Enterprise, destination is github.com)
+  `--sourceHost [SOURCE_HOST_URL], -sh [SOURCE_HOST_URL]`: The GitHub domain to migrate from. Defaults to https://github.2ndsiteinc.com.
+  
+  `--destinationHost [DESTINATION_HOST_URL], -dh [DESTINATION_HOST_URL]`: The GitHub domain to migrate to. Defaults to https://www.github.com. 
 
-  `--clone, -c`: Toggle on clone source repository to destination.
+#### GitHub Action options
 
+##### Clone the GitHub repository
+  `--repo, -r`: Creates new private repository on destination GitHub and clones git commits/branches/tags to the destination. It requires username and token for source and destination. It includes --clone and --githubData options.
+
+##### Copy GitHub data (Milestones/Labels/Issues)
+  `--githubData, -gd`: Migrates GitHub data (Milestones/Labels/Issues). It requires username and token for source and destination.
+
+##### Conle the repository to empty GitHub repository (commits/branches/tags)
+  `--clone, -c`: Clones source repository commits/branchs/tags to the destination. It requires username and token for source and destination unless ssh connection (-s) option.
+
+#### Credential options
+  `--sourceUserName, '-sun`; Username for source GitHub. Mandatory for create repo (-r), migrade GitHub Data (-gd) options.
+
+  `--sourceToken -st`: Your personal access token for the source GitHub account. Mandatory for create repo (-r), migrade GitHub Data (-gd) options.
+
+  `--destinationUserName -dun`: Username for destination GitHub. Mandatory for create repo (-r), migrade GitHub Data (-gd) options.
+
+  `--destinationToken -dt`: Your personal access token for the destination GitHub account. Mandatory for create repo (-r), migrade GitHub Data (-gd) options.
+
+  `--ssh, -s`: Enable SSH connection for destination github. It uses ssh keys for cloning repository. If it is specified with --clone options, source/destination username and password are not required
+
+#### Others
   `--inheritVisibility, -v`: Inherit visibility of source repository. (default is private) It works only with -r option.
+
+  `-h, --help`: show help message and exit.
--- git-mover.py
+++ git-mover.py
@@ -108,14 +108,16 @@ def create_repository(repo, destination_url, destination, credentials, inheritVi
 	url = destination_url+"orgs/"+destinations[0]+"/repos"
 	privateRepo = True if not inheritVisibility else repo['private']
 	repository = {"name": destinations[1], "description": repo['description'], 'homepage': repo['homepage'], 'private': privateRepo, 'has_projects': repo['has_projects'], 'auto_init': False}
-	r = post_req(url, json.dumps(repository), credentials)
-	status = check_res(r)
-	if status:
-		#if the POST request succeeded, parse and store the new milestone returned from GitHub
-		return json.loads(r.text)
-	else:
-		print status
-	return {}
+	print 'create_repository:' + url
+	print 'create_repository: json = ' + json.dumps(repository)
+	# r = post_req(url, json.dumps(repository), credentials)
+	# status = check_res(r)
+	# if status:
+	# 	#if the POST request succeeded, parse and store the new milestone returned from GitHub
+	# 	return json.loads(r.text)
+	# else:
+	# 	print status
+	# return {}
 
 '''
 INPUT:
@@ -125,19 +127,26 @@ INPUT:
 OUTCOME: Post new repository to GitHub
 OUTPUT: A dict of new repository information
 '''
-def clone_repository(source_root, source_repo, source_credentials, destination_root, destination_repo, destination_credentials):
+def clone_repository(source_root, source_repo, source_credentials, destination_root, destination_repo, destination_credentials, ssh):
 	github_api_host = 'api.github.com'
 	source = urlparse(source_root)
 	source_host = 'github.com' if source.netloc.lower() == github_api_host else source.netloc[::-1].replace(ghe_api_path[::-1], '' , 1)[::-1]
-	sourceUrl = source.scheme+'://'+source_credentials['user_name']+':'+source_credentials['token']+'@'+source_host+source.path.replace(ghe_api_path, '', 1)+source_repo+'.git'
+	sourceUrl = ('git@'+source_host+source.path.replace(ghe_api_path, '', 1))[:-1]+':'+source_repo+'.git'
+	if not ssh:
+		sourceUrl = source.scheme+'://'+source_credentials['user_name']+':'+source_credentials['token']+'@'+source_host+source.path.replace(ghe_api_path, '', 1)+source_repo+'.git'
+
 	destination = urlparse(destination_root)
 	destination_host = 'github.com' if destination.netloc.lower() == github_api_host else destination.netloc[::-1].replace(ghe_api_path[::-1], '' , 1)[::-1]
-	destinationUrl = destination.scheme+'://'+destination_credentials['user_name']+':'+destination_credentials['token']+'@'+destination_host+destination.path.replace(ghe_api_path, '', 1)+destination_repo+'.git'
+	destinationUrl = ('git@'+destination_host+destination.path.replace(ghe_api_path, '', 1))[:-1]+':'+destination_repo+'.git'
+	if not ssh:
+		destinationUrl = destination.scheme+'://'+destination_credentials['user_name']+':'+destination_credentials['token']+'@'+destination_host+destination.path.replace(ghe_api_path, '', 1)+destination_repo+'.git'
+
 	current_directory = os.getcwd()
 	temp_dir = tempfile.mkdtemp()
 	os.chdir(temp_dir)
-	cmd = os.system("git clone --bare "+sourceUrl+" .")
-	cmd |= os.system("git push --mirror "+destinationUrl)
+
+	cmd = os.system('git clone --bare '+sourceUrl+' .')
+	cmd |= os.system('git push --mirror '+destinationUrl)
 	os.chdir(current_directory)
 	shutil.rmtree(temp_dir)
 	return cmd == 0
@@ -156,15 +165,17 @@ def create_milestones(milestones, destination_url, destination, credentials):
 	for milestone in milestones:
 		#create a new milestone that includes only the attributes needed to create a new milestone
 		milestone_prime = {"title": milestone["title"], "state": milestone["state"], "description": milestone["description"], "due_on": milestone["due_on"]}
-		r = post_req(url, json.dumps(milestone_prime), credentials)
-		status = check_res(r)
-		if status:
-			#if the POST request succeeded, parse and store the new milestone returned from GitHub
-			returned_milestone = json.loads(r.text)
-			#map the original source milestone's number to the newly created milestone's number
-			milestone_map[milestone['number']] = returned_milestone['number']
-		else:
-			print status
+		print 'Create Milestones: ' + url
+		print 'Create Milestones: json ' + json.dumps(milestone_prime)
+		# r = post_req(url, json.dumps(milestone_prime), credentials)
+		# status = check_res(r)
+		# if status:
+		# 	#if the POST request succeeded, parse and store the new milestone returned from GitHub
+		# 	returned_milestone = json.loads(r.text)
+		# 	#map the original source milestone's number to the newly created milestone's number
+		# 	milestone_map[milestone['number']] = returned_milestone['number']
+		# else:
+		# 	print status
 	return milestone_map
 
 '''
@@ -187,8 +198,10 @@ def create_labels(labels, destination_url, destination, credentials):
 		#If it does, don't add it.
 		if label["name"] not in existing_labels:
 			label_prime = {"name": label["name"], "color": label["color"]}
-			r = post_req(url, json.dumps(label_prime), credentials)
-			status = check_res(r)
+			print 'Create labels: ' + url
+			print 'Create labels: json ' + json.dumps(label_prime)
+			# r = post_req(url, json.dumps(label_prime), credentials)
+			# status = check_res(r)
 
 '''
 INPUT:
@@ -217,71 +230,88 @@ def create_issues(issues, destination_url, destination, milestones, labels, mile
 		#if labels were migrated and the issue to be migrated contains labels
 		if labels and "labels" in issue:
 			issue_prime["labels"] = issue["labels"]
-		r = post_req(url, json.dumps(issue_prime), credentials)
-		status = check_res(r)
-		#if adding the issue failed
-		if not status:
-			#get the message from the response
-			message = json.loads(r.text)
-			#if the error message is for an invalid entry because of the assignee field, remove it and repost with no assignee
-			if message['errors'][0]['code'] == 'invalid' and message['errors'][0]['field'] == 'assignee':
-				sys.stderr.write("WARNING: Assignee "+message['errors'][0]['value']+" on issue \""+issue_prime['title']+"\" does not exist in the destination repository. Issue added without assignee field.\n\n")
-				issue_prime.pop('assignee')
-				post_req(url, json.dumps(issue_prime), credentials)
+		print 'Create issue: ' + url
+		print 'Create issue: json ' + json.dumps(issue_prime)
+		# r = post_req(url, json.dumps(issue_prime), credentials)
+		# status = check_res(r)
+		# #if adding the issue failed
+		# if not status:
+		# 	#get the message from the response
+		# 	message = json.loads(r.text)
+		# 	#if the error message is for an invalid entry because of the assignee field, remove it and repost with no assignee
+		# 	if message['errors'][0]['code'] == 'invalid' and message['errors'][0]['field'] == 'assignee':
+		# 		sys.stderr.write("WARNING: Assignee "+message['errors'][0]['value']+" on issue \""+issue_prime['title']+"\" does not exist in the destination repository. Issue added without assignee field.\n\n")
+		# 		issue_prime.pop('assignee')
+		# 		post_req(url, json.dumps(issue_prime), credentials)
 
 
 def main():
 	parser = argparse.ArgumentParser(description='Migrate Milestones, Labels, and Issues between two GitHub repositories. To migrate a subset of elements (Milestones, Labels, Issues), use the element specific flags (--milestones, --lables, --issues). Providing no flags defaults to all element types being migrated.')
-	parser.add_argument('user_name', type=str, help='Your GitHub (public or enterprise) username: name@email.com')
-	parser.add_argument('token', type=str, help='Your GitHub (public or enterprise) personal access token')
-	parser.add_argument('source_repo', type=str, help='the team and repo to migrate from: <team_name>/<repo_name>')
-	parser.add_argument('destination_repo', type=str, help='the team and repo to migrate to: <team_name>/<repo_name>')
-	parser.add_argument('--destinationToken', '-dt', nargs='?', type=str, help='Your personal access token for the destination account, if you are migrating between GitHub installations')
-	parser.add_argument('--destinationUserName', '-dun', nargs='?', type=str, help='Username for destination account, if you are migrating between GitHub installations')
-	parser.add_argument('--sourceRoot', '-sr', nargs='?', default=ghe_url, type=str, help='The GitHub domain to migrate from. Defaults to '+ghe_url+'. For GitHub enterprise customers, enter the domain for your GitHub installation.')
-	parser.add_argument('--destinationRoot', '-dr', nargs='?', default=github_url, type=str, help='The GitHub domain to migrate to. Defaults to '+github_url+'. For GitHub enterprise customers, enter the domain for your GitHub installation.')
-	parser.add_argument('--milestones', '-m', action="store_true", help='Toggle on Milestone migration.')
-	parser.add_argument('--labels', '-l', action="store_true", help='Toggle on Label migration.')
-	parser.add_argument('--issues', '-i', action="store_true", help='Toggle on Issue migration.')
-	parser.add_argument('--repo', '-r', action="store_true", help='Toggle on create new repository on destination GitHub.')
-	parser.add_argument('--clone', '-c', action="store_true", help='Toggle on clone source repository to destination.')
-	parser.add_argument('--inheritVisibility', '-v', action="store_true", help='Inherit visibility of source repository. It works only with -r option.')
+	parser.add_argument('source_repo', type=str, help='the owner and repo to migrate from: <owner>/<repo_name>')
+	parser.add_argument('destination_repo', type=str, help='the owner and repo to migrate to: <owner>/<repo_name>')
+
+	parser.add_argument('--repo', '-r', action="store_true", help='Creates new private repository on destination GitHub and clones git commits/branches/tags to the destination. It requires username and token for source and destination.')
+	parser.add_argument('--githubData', '-gd', action="store_true", help='Migrates GitHub data (Milestones/Labels/Issues). It requires username and token for source and destination.')
+	parser.add_argument('--clone', '-c', action="store_true", help='Clones source repository commits/branchs/tags to the destination. It requires username and token for source and destination unless ssh connection (-s) option.')
+
+	parser.add_argument('--sourceUserName', '-sun', nargs='?', type=str, action='store', dest='sourceUserName', help='Username for source GitHub. Mandatory for create repo (-r), migrade GitHub Data (-gd) options.')	
+	parser.add_argument('--sourceToken', '-st', nargs='?', type=str, action='store', dest='sourceToken', help='Your personal access token for the source GitHub account. Mandatory for create repo (-r), migrade GitHub Data (-gd) options.')
+	parser.add_argument('--destinationUserName', '-dun', nargs='?', type=str, action='store', dest='destinationUserName', help='Username for destination GitHub. Mandatory for create repo (-r), migrade GitHub Data (-gd) options.')
+	parser.add_argument('--destinationToken', '-dt', nargs='?', type=str, action='store', dest='destinationToken', help='Your personal access token for the destination GitHub account. Mandatory for create repo (-r), migrade GitHub Data (-gd) options.')
+	parser.add_argument('--ssh', '-s', action="store_true", help='Enable SSH connection for destination github.')
+
+	parser.add_argument('--sourceHost', '-sh', nargs='?', default=ghe_url, type=str, action='store', dest='sourceHost', help='The GitHub host to migrate from. Default is '+ghe_url+'.')
+	parser.add_argument('--destinationHost', '-dh', nargs='?', default=github_url, type=str, action='store', dest='destinationHost', help='The GitHub domain to migrate to. Defaults is '+github_url+'.')
+	
+	parser.add_argument('--inheritVisibility', '-v', action="store_true", help='Inherit visibility (public/private repository) of source repository. By default -r option creates private repository. It works only with -r option.')
+	
+
 
 	args = parser.parse_args()
 
 	destination_repo = args.destination_repo
 	source_repo = args.source_repo
-	source_credentials = {'user_name' : args.user_name, 'token' : args.token}
+	
+	if (args.sourceHost == github_url):
+		args.sourceHost = github_api_url
 
-	if (args.sourceRoot == github_url):
-		args.sourceRoot = github_api_url
+	if (args.destinationHost == github_url):
+		args.destinationHost = github_api_url
 
-	if (args.destinationRoot == github_url):
-		args.destinationRoot = github_api_url
+	if (args.sourceHost != github_api_url) and not args.sourceHost.endswith(ghe_api_path):
+		args.sourceHost += ghe_api_path
 
-	if (args.sourceRoot != github_api_url) and not args.sourceRoot.endswith(ghe_api_path):
-		args.sourceRoot += ghe_api_path
+	if (args.destinationHost != github_api_url) and not args.destinationHost.endswith(ghe_api_path):
+		args.destinationHost += ghe_api_path
 
-	if (args.destinationRoot != github_api_url) and not args.destinationRoot.endswith(ghe_api_path):
-		args.destinationRoot += ghe_api_path
+	if not args.repo and not args.clone and not args.githubData:
+		sys.stderr.write('GitHub action option is not specified. Either --repo, --clone, --githubData option is required.\n')
+		quit()
 
-	if (args.sourceRoot != args.destinationRoot):
-		if not (args.destinationToken):
-			sys.stderr.write("Error: Source and Destination Roots are different but no token was supplied for the destination repo.")
-			quit()
+	if (args.repo or args.githubData) and not (args.sourceUserName and args.sourceToken and args.destinationUserName and args.destinationToken):
+		sys.stderr.write('GitHub source and destination user name and token are required.\n')
+		quit()
 
-	if not (args.destinationUserName):
-		print('No destination User Name provided, defaulting to source User Name: '+args.user_name)
-		args.destinationUserName = args.user_name
+	if args.clone and not args.ssh and not (args.sourceUserName and args.sourceToken and args.destinationUserName and args.destinationToken):
+		sys.stderr.write('GitHub source and destination user name and token are required.\n')
+		quit()
 
+	source_root = args.sourceHost
+	if not source_root.endswith('/'):
+		source_root = source_root+'/'
+
+	destination_root = args.destinationHost
+	if not destination_root.endswith('/'):
+		destination_root = destination_root+'/'
+
+	source_credentials = {'user_name': args.sourceUserName, 'token': args.sourceToken}
 	destination_credentials = {'user_name': args.destinationUserName, 'token': args.destinationToken}
 
-	source_root = args.sourceRoot+'/'
-	destination_root = args.destinationRoot+'/'
 
 	if args.repo:
 		repo = download_repository(source_root, source_repo, source_credentials)
 		args.clone = True
+		args.githubData = True
 		if repo:
 			res = create_repository(repo, destination_root, destination_repo, destination_credentials, args.inheritVisibility)
 		else:
@@ -289,19 +319,14 @@ def main():
 			quit()
 
 	if args.clone:
-		clone = clone_repository(source_root, source_repo, source_credentials, destination_root, destination_repo, destination_credentials)
+		ssh = args.ssh
+		clone = clone_repository(source_root, source_repo, source_credentials, destination_root, destination_repo, destination_credentials, ssh)
 		if not clone:
 			sys.stderr.write('ERROR: Failed to clone the repository. Exiting...')
 			quit()
 
-	milestone_map = None
-
-	if args.milestones == False and args.labels == False and args.issues == False:
-		args.milestones = True
-		args.labels = True
-		args.issues = True
-
-	if args.milestones:
+	if args.githubData:
+		milestone_map = None
 		milestones = download_milestones(source_root, source_repo, source_credentials)
 		if milestones:
 			milestone_map = create_milestones(milestones, destination_root, destination_repo, destination_credentials)
@@ -310,8 +335,7 @@ def main():
 			quit()
 		else:
 			print "No milestones found. None migrated"
-
-	if args.labels:
+		
 		labels = download_labels(source_root, source_repo, source_credentials)
 		if labels:
 			res = create_labels(labels, destination_root, destination_repo, destination_credentials)
@@ -320,12 +344,11 @@ def main():
 			quit()
 		else:
 			print "No Labels found. None migrated"
-
-	if args.issues:
+		
 		issues = download_issues(source_root, source_repo, source_credentials)
 		if issues:
 			sameInstall = False
-			if (args.sourceRoot == args.destinationRoot):
+			if (args.sourceHost == args.destinationHost):
 				sameInstall = True
 			res = create_issues(issues, destination_root, destination_repo, args.milestones, args.labels, milestone_map, destination_credentials, sameInstall)
 		elif issues == False:
